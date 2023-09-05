9180a566a751173b5746e7af22c06d8311e6c97a
I'm Jeroen <108660153+werjeroen@users.noreply.github.com>
2023-09-05 15:01:56 +0200

    Update plugin-test.php

--- plugin-test.php
+++ plugin-test.php
@@ -1,1 +1,21 @@
+<?php
+/*
+Plugin Name: Custom Message Plugin
+Description: A simple WordPress plugin that displays a custom message.
+Version: 1.0.1
+Author: We'r Media
+*/
 
+function custom_message_function() {
+    echo '<div class="notice notice-success is-dismissible"><p>Hello, this is a custom message from your WordPress plugin!</p></div>';
+}
+add_action('admin_notices', 'custom_message_function');
+
+if ( is_admin() )  {
+    include_once plugin_dir_path( dirname( __FILE__, 2 ) ); . '/core/config/updater/PluginUpdater.php';
+
+    $updater = new PDUpdater(__FILE__);
+    $updater->set_username('We-R-Media');
+    $updater->set_repository('werpluginupdatecheck');
+    $updater->initialize();
+}
